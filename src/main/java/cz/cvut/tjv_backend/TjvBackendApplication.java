4974a09568eff4e2cb9d17ac9a541b185f115894
DziedzicGrzegorz <grzegorzdziedzic198@gmail.com>
2024-10-18 21:53:57 +0200

    build(spring-boot): add clean spring boot

---
+++ src/main/java/cz/cvut/tjv_backend/TjvBackendApplication.java
@@ -0,0 +1,12 @@
+package cz.cvut.tjv_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TjvBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TjvBackendApplication.class, args);
+    }
+}
---
+++ src/test/java/cz/cvut/tjv_backend/TjvBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package cz.cvut.tjv_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TjvBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
