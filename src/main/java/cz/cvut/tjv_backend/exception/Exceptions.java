f091ea117faa3477f8cf6bb74dbcc8f74b9960e6
DziedzicGrzegorz <grzegorzdziedzic198@gmail.com>
2024-11-10 14:41:50 +0100

    feat(error): exception
    standardize error response

---
+++ src/main/java/cz/cvut/tjv_backend/exception/Exceptions.java
@@ -0,0 +1,23 @@
+package cz.cvut.tjv_backend.exception;
+
+public class Exceptions {
+
+    public static class NotFoundException extends RuntimeException {
+        public NotFoundException(String message) {
+            super(message);
+        }
+    }
+
+    public static class BadRequestException extends RuntimeException {
+        public BadRequestException(String message) {
+            super(message);
+        }
+    }
+
+    public static class UnauthorizedException extends RuntimeException {
+        public UnauthorizedException(String message) {
+            super(message);
+        }
+    }
+
+}
